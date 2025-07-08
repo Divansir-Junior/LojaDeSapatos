97f0304e265b8b82367e54b1c29670c4b9145df1
Divansir-Junior <jscrobut989@gmail.com>
2025-07-08 02:06:56 -0300

    Criando primeira tela de menu

---
+++ src/test/java/Loja/de/Sapatos/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Loja.de.Sapatos.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
