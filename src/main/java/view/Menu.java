f2e6e9ff8b34919fc3a3d0d30b8cb78443197cc7
Divansir-Junior <jscrobut989@gmail.com>
2025-07-08 02:25:42 -0300

    Adicionada cores ao terminal (sujeito a mudançãs) Cores usadas : azul,vermelho,amareloe verde

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -9,20 +9,27 @@ public class Menu {
     private final MenuController controller = new MenuController();
 
     public void ShowMainMenu() {
-        Scanner scanner = new Scanner(System.in);
+        String cyan = "\u001B[36m";
+        String green = "\u001B[32m";
+        String yellow = "\u001B[33m";
+        String reset = "\u001B[0m";
+        String red = "\u001B[31m";
 
-        System.out.println("====================================================================================================");
-        System.out.println("  _   _   ______   _______             _____   _    _    ____    ______ \n" +
+        System.out.println(cyan + "====================================================================================================" + reset);
+        System.out.println(green +
+                "  _   _   ______   _______             _____   _    _    ____    ______ \n" +
                 " | \\ | | |  ____| |__   __|           / ____| | |  | |  / __ \\  |  ____|\n" +
                 " |  \\| | | |__       | |     ______  | (___   | |__| | | |  | | | |__   \n" +
                 " | . ` | |  __|      | |    |______|  \\___ \\  |  __  | | |  | | |  __|  \n" +
                 " | |\\  | | |____     | |              ____) | | |  | | | |__| | | |____ \n" +
-                " |_| \\_| |______|    |_|             |_____/  |_|  |_|  \\____/  |______");
-        System.out.println("====================================================================================================");
-        System.out.println("DIGITE SUA ESCOLHA:");
-        System.out.println("ENTRAR");
-        System.out.println("SAIR");
+                " |_| \\_| |______|    |_|             |_____/  |_|  |_|  \\____/  |______" + reset);
+        System.out.println(cyan + "====================================================================================================" + reset);
+        System.out.println(yellow + "DIGITE SUA ESCOLHA:" + reset);
+        System.out.println(green + "ENTRAR" + reset);
+        System.out.println(red + "SAIR" + reset);
         System.out.print("👉 ");
+        Scanner scanner = new Scanner(System.in);
+
 
         String escolha = scanner.nextLine().trim().toLowerCase();
 
