5a378ccef492bd6edf50a0f0fa014347f89e6d2c
Divansir-Junior <jscrobut989@gmail.com>
2025-07-09 19:26:06 -0300

    Cores adiconadas a classe

--- src/main/java/util/Colors.java
+++ src/main/java/util/Colors.java
@@ -1,4 +1,12 @@
 package util;
 
 public class Colors {
+
+    public static final String CYAN = "\u001B[36m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String RESET = "\u001B[0m";
+    public static final String RED = "\u001B[31m";
+    public static final String BLUE = "\u001B[34m";
+    public static  final String WHITE = "\t\\u001B[37m";
 }
