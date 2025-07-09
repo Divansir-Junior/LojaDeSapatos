155bbe8b1c77baf0a9b02837105567be8037ba43
Divansir-Junior <jscrobut989@gmail.com>
2025-07-09 19:55:17 -0300

    Tela de Saída concluida

--- src/main/java/com/lojaSapatos/view/Exit.java
+++ src/main/java/com/lojaSapatos/view/Exit.java
@@ -5,16 +5,23 @@ import util.Colors;
 public class Exit {
     Colors color = new Colors();
     public void exitMenu() {
-        System.out.println("\n" +
+        System.out.print(Colors.getColor("yellow") + "===========" +
+                "================================================" +  Colors.RESET);
+        System.out.println(Colors.getColor("red") +
+                "\n" +
                 "  ______  __   __  _____   _______   _____   _   _    _____               \n" +
                 " |  ____| \\ \\ / / |_   _| |__   __| |_   _| | \\ | |  / ____|              \n" +
                 " | |__     \\ V /    | |      | |      | |   |  \\| | | |  __               \n" +
                 " |  __|     > <     | |      | |      | |   | . ` | | | |_ |              \n" +
                 " | |____   / . \\   _| |_     | |     _| |_  | |\\  | | |__| |    _   _   _ \n" +
                 " |______| /_/ \\_\\ |_____|    |_|    |_____| |_| \\_|  \\_____|   (_) (_) (_)\n" +
-                "                                                                          \n" +
-                "                                                                          \n" );
-        System.out.println(Colors.getColor("vermelho") + "==================================" + Colors.RED);
+
+                 Colors.RESET);
+        System.out.print(Colors.getColor("yellow") + "===========" +
+                "================================================" + "====" + Colors.RESET);
+
+
+
 
 
     }
