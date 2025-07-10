6e2cd0f5a953b70a6633e5aa104cb62b3e29f926
Divansir-Junior <jscrobut989@gmail.com>
2025-07-10 00:45:15 -0300

    Classe controladora do tenis criada

--- src/main/java/com/lojaSapatos/view/CreateShoe.java
+++ src/main/java/com/lojaSapatos/view/CreateShoe.java
@@ -1,4 +1,15 @@
 package com.lojaSapatos.view;
 
 public class CreateShoe {
+
+    public void createShoeMenu () {
+
+        System.out.println("Digite o nome do sapato");
+        System.out.println("Digite a marca do sapato");
+        System.out.println("Digite o tamanho do sapato");
+        System.out.println("Digite a cor do sapato");
+
+    }
+
+
 }
