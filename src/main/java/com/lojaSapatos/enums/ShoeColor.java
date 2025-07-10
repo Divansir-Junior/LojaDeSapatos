a016f8959e79ef504ba436c2e57ba93332e4f34a
Divansir-Junior <jscrobut989@gmail.com>
2025-07-10 00:57:56 -0300

    Classe de cores dos tênis populada

--- src/main/java/com/lojaSapatos/enums/ShoeColor.java
+++ src/main/java/com/lojaSapatos/enums/ShoeColor.java
@@ -1,4 +1,34 @@
 package com.lojaSapatos.enums;
 
-public class ShoeColor {
+public enum ShoeColor {
+    BLACK("Preto"),
+    BLUE("Azul"),
+    RED("Vermelho"),
+    ORANGE("Laranja"),
+    PURPLE("Roxo"),
+    YELLOW("Amarelo"),
+    GREEN("Verde"),
+    GOLD("Dourado"),
+    BROWN("Marrom"),
+
+    WHITE("Branco"),
+    GRAY("Cinza"),
+    BEIGE("Bege"),
+    PINK("Rosa"),
+    SILVER("Prata"),
+    BURGUNDY("Bordô"),
+    WINE("Vinho"),
+    TURQUOISE("Turquesa"),
+    LILAC("Lilás"),
+    NAVY("Azul Marinho");
+
+    private final String desc;
+
+    ShoeColor(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDescription() {
+        return desc;
+    }
 }
