fc7f6ab5c09ab35a4d14333ea533c87899837644
Divansir-Junior <jscrobut989@gmail.com>
2025-07-11 22:42:51 -0300

    Interface criada

--- src/main/java/com/lojaSapatos/repository/ShoeRepository.java
+++ src/main/java/com/lojaSapatos/repository/ShoeRepository.java
@@ -1,4 +1,11 @@
 package com.lojaSapatos.repository;
 
-public class ShoeRepository {
+import com.lojaSapatos.model.Shoe;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShoeRepository  extends JpaRepository <Shoe,Long> {
+
+
+
+
 }
