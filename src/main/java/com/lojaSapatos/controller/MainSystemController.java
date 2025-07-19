fbff60332deff85c9ae2b53f6e83a4fe8c20543f
Divansir-Junior <jscrobut989@gmail.com>
2025-07-19 03:22:55 -0300

    Refatooração completa do projeto

--- src/main/java/com/lojaSapatos/controller/MainSystemController.java
+++ src/main/java/com/lojaSapatos/controller/MainSystemController.java
@@ -1,62 +1,58 @@
 package com.lojaSapatos.controller;
 
-import com.lojaSapatos.enums.ShoeColor;
-import com.lojaSapatos.model.Shoe;
 import com.lojaSapatos.services.PdfService;
 import com.lojaSapatos.services.ShoeService;
 import com.lojaSapatos.util.LineMaker;
 import com.lojaSapatos.view.MenuView;
 import org.springframework.stereotype.Component;
-
-import java.sql.SQLOutput;
-import java.util.List;
-import java.util.Optional;
 import java.util.Scanner;
+import com.lojaSapatos.interaction.InputHandler;
 
 @Component
 public class MainSystemController {
 
+    private final InputHandler inputHandler;
     private final MenuView menuView;
-    private final ShoeService shoeService;
-    private final Scanner scanner = new Scanner(System.in);
     private  PdfService pdfService ;
     private  LineMaker lineMaker;
 
-    public MainSystemController(MenuView menuView, ShoeService shoeService, LineMaker lineMaker,PdfService pdfService) {
+    public MainSystemController(InputHandler inputHandler, MenuView menuView, LineMaker lineMaker, PdfService pdfService) {
+        this.inputHandler = inputHandler;
         this.menuView = menuView;
-        this.shoeService = shoeService;
+
         this.lineMaker = lineMaker;
         this.pdfService = pdfService;
     }
-
 
     public void mainOptions(String choice) {
         switch (choice.toLowerCase()) {
             case "create shoe":
             case "1":
-                createShoe();
+                inputHandler.saveShoe();
                 break;
 
             case "search shoe":
             case "2":
-                searchShoe();
+                inputHandler.searchShoe();
                 break;
 
             case "delete shoe":
             case "3":
                 System.out.println("Digite o ID PARA DELETAR:");
-                deleteShoe();
+                inputHandler.deleteShoe();
                 break;
 
             case "list all":
             case "4":
-                System.out.println("📋 Listando todos os sapatos...");
-                findAll();
+                System.out.println("📋 LISTAGEM ");
+                lineMaker.makeLine();
+                inputHandler.findAll();
+                lineMaker.makeLine();
                 break;
 
             case "filter":
             case "5":
-                filterShoe();
+                inputHandler.filterShoe();
                 break;
 
             case "import doc":
@@ -80,89 +76,4 @@ public class MainSystemController {
         }
     }
 
-    private void createShoe() {
-        System.out.print("Nome do sapato: ");
-        String name = scanner.nextLine();
-
-        System.out.println("Cor do sapato : ");
-        String colorShoe  = scanner.nextLine();
-        ShoeColor chosenColor = ShoeColor.valueOf(colorShoe.toUpperCase());
-
-        System.out.println("Marca do tênis : ");
-        String brand = scanner.nextLine();
-
-        System.out.print("Tamanho do sapato: ");
-        int size = Integer.parseInt(scanner.nextLine());
-
-        Shoe shoe = new Shoe(name,brand,size);
-        shoe.setShoeColor(chosenColor);
-
-        Shoe saved = shoeService.saveShoe(shoe);
-        System.out.println("✅ Sapato criado com ID: " + saved.getId());
-    }
-
-    private void searchShoe() {
-        System.out.println("Nome de busca :");
-        String nameSearch = scanner.nextLine();
-
-        Shoe res = shoeService.searchShoeByName(nameSearch);
-
-        if (res != null) {
-            System.out.println("✅ Tênis achado: " + res);
-
-        }
-        else {
-            System.out.println("❌ Nenhum tênis encontrado com esse nome.");
-        }
-        System.out.println(lineMaker.makeLine());
-    }
-
-    private void findAll() {
-        List<Shoe> sapatos = shoeService.listAll();
-
-        if (sapatos.isEmpty()) {
-            System.out.println("ERRO");
-            System.out.println("📭 Nenhum sapato encontrado.");
-        } else {
-            System.out.println("📋 Lista de sapatos encontrados:");
-            for (Shoe s : sapatos) {
-                System.out.println(s); // usa o toString da entidade
-            }
-        }
-    }
-
-    private boolean deleteShoe() {
-        Long id = scanner.nextLong();
-        shoeService.deleteShoe(id);
-        return true;
-    }
-
-    private void filterShoe() {
-        System.out.println("\n📦 Filtro de Tênis");
-
-        System.out.print("🔤 Nome: ");
-        String name = scanner.nextLine().trim();
-
-        System.out.print("🏷️ Marca: ");
-        String brand = scanner.nextLine().trim();
-
-        System.out.print("📏 Tamanho do tênis: ");
-        int size = scanner.nextInt();
-        scanner.nextLine(); // limpar quebra de linha
-
-        System.out.println("\n🔎 Buscando tênis com os critérios informados...\n");
-
-        Optional<Shoe> resultado = shoeService.filterShoe(name, brand, size);
-
-        resultado.ifPresentOrElse(shoe -> {
-            System.out.println("✅ Tênis encontrado:");
-            System.out.printf("  🆔 ID     : %d%n", shoe.getId());
-            System.out.printf("  👟 Nome   : %s%n", shoe.getName());
-            System.out.printf("  🏷️ Marca  : %s%n", shoe.getBrand());
-            System.out.printf("  📏 Tamanho: %d%n", shoe.getSize());
-
-        }, () -> {
-            System.out.println("❌ Nenhum tênis encontrado com os critérios informados.");
-        });
-    }
 }
---
+++ src/main/java/com/lojaSapatos/interaction/InputHandler.java
@@ -0,0 +1,110 @@
+package com.lojaSapatos.interaction;
+import java.util.List;
+import java.util.Optional;
+import java.util.Scanner;
+import com.lojaSapatos.enums.ShoeColor;
+import com.lojaSapatos.model.Shoe;
+import com.lojaSapatos.services.ShoeService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InputHandler {
+
+    private final Scanner sc = new Scanner(System.in);
+
+    private final ShoeService shoeService;
+
+    public InputHandler(ShoeService shoeService) {
+
+        this.shoeService = shoeService;
+    }
+
+    public void saveShoe(){
+        System.out.println("Digite o nome do tênis :");
+        String name = sc.nextLine();
+
+        System.out.println("Cor do sapato:");
+        String colorInput = sc.nextLine().toUpperCase();
+        ShoeColor shoeColor = ShoeColor.valueOf(colorInput);
+
+        System.out.println("Marca do sapato :");
+        String brand = sc.nextLine();
+
+        System.out.println("Número do tênis : ");
+        int size = Integer.parseInt(sc.nextLine());
+
+        Shoe shoe = new Shoe(name, brand, size);
+        shoe.setShoeColor(shoeColor);
+        shoeService.saveShoe(shoe);
+
+    }
+
+    public boolean searchShoe() {
+        System.out.println("Nome do tênis : ");
+        String nameSearch = sc.nextLine().trim();
+
+        Shoe res = shoeService.searchShoeByName(nameSearch);
+        if (res != null) {
+            System.out.println("Tênis encontrado : " + res);
+            return true;
+        }
+        System.out.println("Nenhum tÊnis encontrado com esse nome");
+        return false;
+    }
+
+    public void findAll() {
+        List<Shoe> shoes = shoeService.listAll();
+        if (shoes.isEmpty()) {
+            System.out.println("Lista vazia");
+        } else {
+            System.out.println("Lista de tênis:");
+            shoes.forEach(shoe -> System.out.println(shoe));
+        }
+    }
+
+    public boolean deleteShoe() {
+        try {
+            Long id = sc.nextLong();
+            sc.nextLine();
+
+            shoeService.deleteShoe(id);
+            System.out.println("Tênis deletado");
+            return true;
+        } catch (Exception e) {
+            System.out.println("Erro" + e);
+            sc.nextLine();
+            return false;
+        }
+    }
+
+    public boolean filterShoe() {
+        System.out.println("Filtro de tÊnis : ");
+
+        System.out.println("Nome : ");
+        String filterName = sc.nextLine();
+
+        System.out.println("Marca : ");
+        String filterBrand = sc.nextLine();
+
+        System.out.println("Tamanho : ");
+        int filterSize = sc.nextInt();
+
+        System.out.println("BUSCANDO TÊNIS ....");
+
+        Optional<Shoe> filterResult = shoeService.filterShoe(filterName, filterBrand, filterSize);
+
+        if (filterResult.isPresent()) {
+            Shoe shoe = filterResult.get();
+            System.out.println("✅ Tênis encontrado:");
+            System.out.printf("  🆔 ID     : %d%n", shoe.getId());
+            System.out.printf("  👟 Nome   : %s%n", shoe.getName());
+            System.out.printf("  🏷️ Marca  : %s%n", shoe.getBrand());
+            System.out.printf("  📏 Tamanho: %d%n", shoe.getSize());
+            return true;
+        } else {
+            System.out.println("❌ Nenhum tênis encontrado com os critérios informados.");
+            return false;
+        }
+    }
+
+}
